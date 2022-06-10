c3c5c11836771d7d433b6535d29919feb6261cc3
Marek Barszcz <marek.barszcz@fortedigital.no>
2022-06-10 14:55:51 +0200

    Merge two related css helpers files

--- EpiResponsivePicture.Tests/ImageTransformationHelperTests.cs
+++ EpiResponsivePicture.Tests/ImageTransformationHelperTests.cs
@@ -9,6 +9,7 @@ using Moq;
 using NUnit.Framework;
 using Forte.EpiResponsivePicture.ResizedImage;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using static Forte.EpiResponsivePicture.ResizedImage.CssHelpers;
 
 namespace Forte.EpiResponsivePicture.Tests
 {
@@ -52,7 +53,7 @@ namespace Forte.EpiResponsivePicture.Tests
 
             Assert.That(markup,
                 Is.EqualTo(
-                    "<picture><source media=\"(min-width:1900px)\" sizes=\"90vw\" srcset=\"/images/foo.jpg?width=1900&amp;height=1425&amp;quality=90&amp;rxy=0.5,0.5 1900w, /images/foo.jpg?width=2400&amp;height=1800&amp;quality=90&amp;rxy=0.5,0.5 2400w\" /><img alt=\"\" src=\"/images/foo.jpg\" /></picture>"));
+                    "<picture><source media=\"(min-width: 1900px)\" sizes=\"90vw\" srcset=\"/images/foo.jpg?width=1900&amp;height=1425&amp;quality=80&amp;rxy=0.5,0.5 1900w, /images/foo.jpg?width=2400&amp;height=1800&amp;quality=80&amp;rxy=0.5,0.5 2400w\" /><img alt=\"\" src=\"/images/foo.jpg\" /></picture>"));
         }
 
         [Test]
@@ -65,7 +66,7 @@ namespace Forte.EpiResponsivePicture.Tests
 
             Assert.That(markup,
                 Is.EqualTo(
-                    "<picture><source media=\"(min-width:1900px)\" sizes=\"90vw\" srcset=\"/images/foo.jpg?width=1900&amp;height=1425&amp;quality=90&amp;format=JPEG&amp;rxy=0.5,0.5 1900w, /images/foo.jpg?width=2400&amp;height=1800&amp;quality=90&amp;format=JPEG&amp;rxy=0.5,0.5 2400w\" /><img alt=\"\" src=\"/images/foo.jpg\" /></picture>"));
+                    "<picture><source media=\"(min-width: 1900px)\" sizes=\"90vw\" srcset=\"/images/foo.jpg?width=1900&amp;height=1425&amp;quality=80&amp;format=JPEG&amp;rxy=0.5,0.5 1900w, /images/foo.jpg?width=2400&amp;height=1800&amp;quality=80&amp;format=JPEG&amp;rxy=0.5,0.5 2400w\" /><img alt=\"\" src=\"/images/foo.jpg\" /></picture>"));
         }
 
         private static readonly PictureProfile profileOverridingFormat = new PictureProfile()
@@ -75,12 +76,12 @@ namespace Forte.EpiResponsivePicture.Tests
             {
                 new PictureSource()
                 {
-                    MediaCondition = "(min-width:1900px)",
+                    MediaCondition = MediaQueryMinWidth(1900),
                     AllowedWidths = new[] {1900, 2400},
                     TargetAspectRatio = AspectRatio.Create(1),
                     Sizes = new[]
                     {
-                        "90vw",
+                        Size((90, Unit.Vw)),
                     },
                 },
             },
@@ -92,11 +93,11 @@ namespace Forte.EpiResponsivePicture.Tests
             {
                 new PictureSource()
                 {
-                    MediaCondition = "(min-width:1900px)",
+                    MediaCondition = MediaQueryMinWidth(1900),
                     AllowedWidths = new[] {1900, 2400},
                     Sizes = new[]
                     {
-                        "90vw",
+                        Size((90, Unit.Vw)),
                     },
                 },
             },
--- EpiResponsivePicture/ResizedImage/CssHelpers.cs
+++ EpiResponsivePicture/ResizedImage/CssHelpers.cs
@@ -1,7 +1,13 @@
+using System;
+using System.Globalization;
+
 namespace Forte.EpiResponsivePicture.ResizedImage
 {
     public static class CssHelpers
     {
+        private const string Max = "max";
+        private const string Min = "min";
+        
         public enum Unit
         {
             Px = 0,
@@ -15,7 +21,38 @@ namespace Forte.EpiResponsivePicture.ResizedImage
             Vw,
             Percent,
         }
+        
+        public static string MediaQueryMaxWidth(double queryWidthPixel) => BuildMediaQueryString(Max, (queryWidthPixel, Unit.Px));
+        
+        public static string MediaQueryMaxWidth(ValueTuple<double, Unit> queryWidth) => BuildMediaQueryString(Max, queryWidth);
+        
+        public static string MediaQueryMaxWidthWithSize(double queryWidthPixel, double imageWidthPixel) => 
+            BuildMediaQueryWithSizeString(Max, (queryWidthPixel, Unit.Px), (imageWidthPixel, Unit.Px));
 
+        public static string MediaQueryMaxWidthWithSize(ValueTuple<double, Unit> queryWidth, ValueTuple<double, Unit> imageWidth)
+            => BuildMediaQueryWithSizeString(Max, queryWidth, imageWidth);
+        
+        public static string MediaQueryMinWidth(double queryWidthPixel) => BuildMediaQueryString(Min, (queryWidthPixel, Unit.Px));
+        
+        public static string MediaQueryMinWidth(ValueTuple<double, Unit> queryWidth) => BuildMediaQueryString(Min, queryWidth);
+        
+        public static string MediaQueryMinWidthWithSize(double queryWidthPixel, double imageWidthPixel) => 
+            BuildMediaQueryWithSizeString(Min, (queryWidthPixel, Unit.Px), (imageWidthPixel, Unit.Px));
+
+        public static string MediaQueryMinWidthWithSize(ValueTuple<double, Unit> queryWidth, ValueTuple<double, Unit> width)
+            => BuildMediaQueryWithSizeString(Min, queryWidth, width);
+
+        public static string SizePixel(double widthPixel) => BuildSize((widthPixel, Unit.Px));
+        public static string Size(ValueTuple<double, Unit> width) => BuildSize(width);
+        
+        private static string BuildMediaQueryWithSizeString(string mediaQueryType, ValueTuple<double, Unit> queryWidth, ValueTuple<double, Unit> width) => 
+            $"{BuildMediaQueryString(mediaQueryType, queryWidth)} {BuildSize(width)}";
+
+        private static string BuildMediaQueryString(string mediaQueryType, ValueTuple<double, Unit> queryWidth) =>
+            $"({mediaQueryType}-width: {queryWidth.Item1.ToString(CultureInfo.InvariantCulture)}{queryWidth.Item2.AsString()})";
+
+        private static string BuildSize(ValueTuple<double, Unit> width) => $"{width.Item1.ToString(CultureInfo.InvariantCulture)}{width.Item2.AsString()}";
+        
         internal static string AsString(this Unit unit) => unit switch
         {
             Unit.Percent => "%",
