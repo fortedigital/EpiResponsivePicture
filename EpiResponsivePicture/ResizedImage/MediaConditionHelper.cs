9d2c2c35c5498caa9f0ad9fbfdf86a4d180fcd18
Marek Barszcz <marek.barszcz@fortedigital.no>
2022-06-10 14:32:05 +0200

    Add more css helpers

--- EpiResponsivePicture/ResizedImage/MediaConditionHelper.cs
+++ EpiResponsivePicture/ResizedImage/MediaConditionHelper.cs
@@ -1,23 +1,44 @@
+using System;
 using System.Globalization;
+using EPiServer.Events;
 using static Forte.EpiResponsivePicture.ResizedImage.CssHelpers;
 
 namespace Forte.EpiResponsivePicture.ResizedImage
 {
-    public static class MediaQueryHelper
+    public static class CssHelper
     {
         private const string Max = "max";
         private const string Min = "min";
 
-        public static string MaxWidth(double queryWidth, double imageWidth, Unit queryWidthUnit = Unit.Px,
-            Unit imageWidthUnit = Unit.Px) =>
-            BuildMediaQueryString(Max, queryWidth, imageWidth, queryWidthUnit, imageWidthUnit);
+        public static string MediaQueryMaxWidth(double queryWidthPixel) => BuildMediaQueryString(Max, (queryWidthPixel, Unit.Px));
         
-        public static string MinWidth(double queryWidth, double imageWidth, Unit queryWidthUnit = Unit.Px,
-            Unit imageWidthUnit = Unit.Px) => 
-            BuildMediaQueryString(Min, queryWidth, imageWidth, queryWidthUnit, imageWidthUnit);
+        public static string MediaQueryMaxWidth(ValueTuple<double, Unit> queryWidth) => BuildMediaQueryString(Max, queryWidth);
         
-        private static string BuildMediaQueryString(string mediaQueryType, double queryWidth, double imageWidth,
-            Unit queryWidthUnit, Unit imageWidthUnit) => 
-            $"({mediaQueryType}-width: {queryWidth.ToString(CultureInfo.InvariantCulture)}{queryWidthUnit.AsString()}) {imageWidth.ToString(CultureInfo.InvariantCulture)}{imageWidthUnit.AsString()}";
+        public static string MediaQueryMaxWidthWithSize(double queryWidthPixel, double imageWidthPixel) => 
+            BuildMediaQueryWithSizeString(Max, (queryWidthPixel, Unit.Px), (imageWidthPixel, Unit.Px));
+
+        public static string MediaQueryMaxWidthWithSize(ValueTuple<double, Unit> queryWidth, ValueTuple<double, Unit> imageWidth)
+            => BuildMediaQueryWithSizeString(Max, queryWidth, imageWidth);
+        
+        public static string MediaQueryMinWidth(double queryWidthPixel) => BuildMediaQueryString(Min, (queryWidthPixel, Unit.Px));
+        
+        public static string MediaQueryMinWidth(ValueTuple<double, Unit> queryWidth) => BuildMediaQueryString(Min, queryWidth);
+        
+        public static string MediaQueryMinWidthWithSize(double queryWidthPixel, double imageWidthPixel) => 
+            BuildMediaQueryWithSizeString(Min, (queryWidthPixel, Unit.Px), (imageWidthPixel, Unit.Px));
+
+        public static string MediaQueryMinWidthWithSize(ValueTuple<double, Unit> queryWidth, ValueTuple<double, Unit> width)
+            => BuildMediaQueryWithSizeString(Min, queryWidth, width);
+
+        public static string SizePixel(double widthPixel) => BuildSize((widthPixel, Unit.Px));
+        public static string Size(ValueTuple<double, Unit> width) => BuildSize(width);
+        
+        private static string BuildMediaQueryWithSizeString(string mediaQueryType, ValueTuple<double, Unit> queryWidth, ValueTuple<double, Unit> width) => 
+            $"{BuildMediaQueryString(mediaQueryType, queryWidth)} {BuildSize(width)}";
+
+        private static string BuildMediaQueryString(string mediaQueryType, ValueTuple<double, Unit> queryWidth) =>
+            $"({mediaQueryType}-width: {queryWidth.Item1.ToString(CultureInfo.InvariantCulture)}{queryWidth.Item2.AsString()})";
+
+        private static string BuildSize(ValueTuple<double, Unit> width) => $"{width.Item1.ToString(CultureInfo.InvariantCulture)}{width.Item2.AsString()}";
     }
 }
