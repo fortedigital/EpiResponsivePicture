a0143c1cabb2962c5bda1eed80c7ba6a85d73f0d
Marek Barszcz <marek.barszcz@fortedigital.no>
2022-06-15 15:34:09 +0200

    Init PictureSource and PictureProfile with default values

--- EpiResponsivePicture/ResizedImage/PictureProfile.cs
+++ EpiResponsivePicture/ResizedImage/PictureProfile.cs
@@ -7,5 +7,5 @@ public class PictureProfile
     public int DefaultWidth { get; init; }
     public int MaxImageDimension { get; init; } = 3200; // Default max served by ImageResizer TODO check how it is in ImageSharp
     public ResizedImageFormat Format { get; init; } = ResizedImageFormat.Preserve;
-    public ImmutableArray<PictureSource> Sources { get; init; }
+    public ImmutableArray<PictureSource> Sources { get; init; } = ImmutableArray<PictureSource>.Empty;
 }
--- EpiResponsivePicture/ResizedImage/PictureSource.cs
+++ EpiResponsivePicture/ResizedImage/PictureSource.cs
@@ -5,9 +5,9 @@ namespace Forte.EpiResponsivePicture.ResizedImage;
 public class PictureSource
 {
     public string MediaCondition { get; init; }
-    public ImmutableArray<int> AllowedWidths { get; init; }
-    public ImmutableArray<string> Sizes { get; init; }
+    public ImmutableArray<int> AllowedWidths { get; init; } = ImmutableArray<int>.Empty;
+    public ImmutableArray<string> Sizes { get; init; } = ImmutableArray<string>.Empty;
     public ScaleMode Mode { get; init; }
-    public AspectRatio TargetAspectRatio { get; init; }
+    public AspectRatio TargetAspectRatio { get; init; } = AspectRatio.Default;
     public int? Quality { get; init; }
 }
