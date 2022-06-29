a3cd295b8d700f7dc60006fe029ea513c2b27ad0
Marek Barszcz <marek.barszcz@fortedigital.no>
2022-06-29 09:29:11 +0200

    Remove todo

--- EpiResponsivePicture/ResizedImage/PictureProfile.cs
+++ EpiResponsivePicture/ResizedImage/PictureProfile.cs
@@ -5,7 +5,7 @@ namespace Forte.EpiResponsivePicture.ResizedImage;
 public class PictureProfile
 {
     public int DefaultWidth { get; init; }
-    public int MaxImageDimension { get; init; } = 3200; // Default max served by ImageResizer TODO check how it is in ImageSharp
+    public int MaxImageDimension { get; init; } = 3200;
     public ResizedImageFormat Format { get; init; } = ResizedImageFormat.Preserve;
     public ImmutableArray<PictureSource> Sources { get; init; } = ImmutableArray<PictureSource>.Empty;
 }
