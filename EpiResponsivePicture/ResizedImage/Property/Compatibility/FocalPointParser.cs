717bc5f04f0e1e141d490d7a3b7ccc304a077eeb
adrian smyda <adrian.smyda@fortedigital.no>
2022-12-13 13:14:20 +0100

    Changed the assembly name to Fore.EpiResponsivePicture. Changed some smaller bugs here and there. Next step: Migration for the assembly name for EPI is need to add.

--- EpiResponsivePicture/ResizedImage/Property/Compatibility/FocalPointParser.cs
+++ EpiResponsivePicture/ResizedImage/Property/Compatibility/FocalPointParser.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Forte.EpiResponsivePicture.Configuration;
-using Microsoft.Extensions.Options;
 
 namespace Forte.EpiResponsivePicture.ResizedImage.Property.Compatibility;
 
