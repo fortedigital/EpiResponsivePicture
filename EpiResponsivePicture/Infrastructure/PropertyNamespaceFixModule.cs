249db574746f6c7f58df9cfbb0cab7dd3ebb9b18
Piotr Gębalski <91609048+Piotrreek@users.noreply.github.com>
2023-01-03 15:10:35 +0100

    Fix/change in assembly naming (#36)
    * Changed the assembly name to Fore.EpiResponsivePicture.
    Changed some smaller bugs here and there.
    Next step: Migration for the assembly name for EPI is need to add.
    
    * Add module for fixing assemlby name
    
    Co-authored-by: adrian smyda <adrian.smyda@fortedigital.no>
    Co-authored-by: Piotr Gębalski <piotr.gebalski@fortedigital.no>

---
+++ EpiResponsivePicture/Infrastructure/PropertyNamespaceFixModule.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using EPiServer.DataAbstraction;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.ServiceLocation;
+using EPiServer.Shell;
+
+namespace Forte.EpiResponsivePicture.Infrastructure
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(ShellInitialization))]
+    public class PropertyNamespaceFixModule : IInitializableModule
+    {
+        public void Initialize(InitializationEngine context)
+        {
+            var repository = context.Locate.Advanced.GetInstance<IPropertyDefinitionTypeRepository>();
+
+            var responsivePictureType = repository
+                .List()
+                .FirstOrDefault(x => x.IsSystemType() == false && x.AssemblyName?.StartsWith("EpiResponsivePicture") == true);
+
+            if (responsivePictureType != null)
+            {
+                var definitionClone = responsivePictureType.CreateWritableClone();
+                definitionClone.AssemblyName = "Forte.EpiResponsivePicture";
+                repository.Save(definitionClone);
+            }
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+        }
+    }
+}
