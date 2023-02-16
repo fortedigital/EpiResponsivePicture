5c1a1936411dca1633b82eea792383d598eeec0d
Piotr Szczygieł <piotr.szczygiel@fortedigital.no>
2023-02-16 16:45:36 +0100

    List replaced by IEnumerable

--- EpiResponsivePicture/Configuration/EpiResponsivePicturesOptions.cs
+++ EpiResponsivePicture/Configuration/EpiResponsivePicturesOptions.cs
@@ -5,5 +5,5 @@ namespace Forte.EpiResponsivePicture.Configuration;
 public class EpiResponsivePicturesOptions
 {
     public bool ImageResizerCompatibilityEnabled { get; set; }
-    public List<string> AdditionalSegments { get; set; }
+    public IEnumerable<string> AdditionalSegments { get; set; }
 }
