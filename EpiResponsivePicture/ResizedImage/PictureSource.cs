6c6da253c6458da49df2e30fb8e62c232b480c3b
AgnieszkaJania <agnieszka.jania@fortedigital.com>
2023-03-06 14:14:14 +0100

    bug fix - adding brackets to media if omitted

--- EpiResponsivePicture/ResizedImage/PictureSource.cs
+++ EpiResponsivePicture/ResizedImage/PictureSource.cs
@@ -1,10 +1,39 @@
+using System;
 using System.Collections.Generic;
+using Org.BouncyCastle.Asn1.Esf;
 
 namespace Forte.EpiResponsivePicture.ResizedImage;
 
 public class PictureSource
 {
-    public string MediaCondition { get; init; }
+    private string _mediaCondition;
+    public string MediaCondition
+    {
+        get
+        {
+            return _mediaCondition;
+        }
+        init
+        {
+            value = value.Trim();
+            if (!value.StartsWith("(") && !value.EndsWith(")"))
+            {
+                _mediaCondition = $"({value})";
+            }else if (!value.StartsWith("("))
+            {
+                _mediaCondition = $"({value}";
+            }
+            else if (!value.EndsWith(")"))
+            {
+                _mediaCondition = $"{value})";
+            }
+            else
+            {
+                _mediaCondition = value;
+            }
+        }
+    }
+
     public IReadOnlyCollection<int> AllowedWidths { get; init; } = new List<int>();
     public IReadOnlyCollection<string> Sizes { get; init; } = new List<string>();
     public ScaleMode Mode { get; init; }
