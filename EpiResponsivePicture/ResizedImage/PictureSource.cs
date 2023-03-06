7964d3cc3411270af2399f130ce781acb5a21db4
AgnieszkaJania <agnieszka.jania@fortedigital.com>
2023-03-06 16:39:09 +0100

    bug fix - addign brackets to media if omitted, applying when used

--- EpiResponsivePicture/ResizedImage/PictureSource.cs
+++ EpiResponsivePicture/ResizedImage/PictureSource.cs
@@ -7,33 +7,7 @@ namespace Forte.EpiResponsivePicture.ResizedImage;
 public class PictureSource
 {
     private string _mediaCondition;
-    public string MediaCondition
-    {
-        get
-        {
-            return _mediaCondition;
-        }
-        init
-        {
-            value = value.Trim();
-            if (!value.StartsWith("(") && !value.EndsWith(")"))
-            {
-                _mediaCondition = $"({value})";
-            }else if (!value.StartsWith("("))
-            {
-                _mediaCondition = $"({value}";
-            }
-            else if (!value.EndsWith(")"))
-            {
-                _mediaCondition = $"{value})";
-            }
-            else
-            {
-                _mediaCondition = value;
-            }
-        }
-    }
-
+    public string MediaCondition { get; init; }
     public IReadOnlyCollection<int> AllowedWidths { get; init; } = new List<int>();
     public IReadOnlyCollection<string> Sizes { get; init; } = new List<string>();
     public ScaleMode Mode { get; init; }
